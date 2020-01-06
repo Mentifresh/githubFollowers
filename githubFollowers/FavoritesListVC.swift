571af7371c894c05e8912ec8801d2e49c296c9c6
Daniel Kilders <danielkildersdiaz@gmail.com>
2020-01-06 16:50:11 +0000

    Base project

---
+++ githubFollowers/FavoritesListVC.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesListVC.swift
+//  githubFollowers
+//
+//  Created by Dani on 04/01/2020.
+//  Copyright © 2020 Dani. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+    }
+}
