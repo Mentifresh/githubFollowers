51ee11c31d7ab19e6a60ca5e1a315c11c8a60e79
Daniel Kilders <danielkildersdiaz@gmail.com>
2020-01-07 18:25:12 +0000

    added networking models and quick bugfixes

---
+++ githubFollowers/Models/Follower.swift
@@ -0,0 +1,14 @@
+//
+//  Follower.swift
+//  githubFollowers
+//
+//  Created by Dani on 07/01/2020.
+//  Copyright © 2020 Dani. All rights reserved.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatar_url: String
+}
