45bc631a4a0ec8e0eda63774a6c5a52945ecc030
Daniel Kilders Diaz <dankil.dev@gmail.com>
2020-01-12 16:20:08 +0100

    Added custom followerCell and avatarImageView

---
+++ githubFollowers/Custom Views/GFAvatarImageView.swift
@@ -0,0 +1,30 @@
+//
+//  GFAvatarImageView.swift
+//  githubFollowers
+//
+//  Created by Daniel Kilders Díaz on 12/01/2020.
+//  Copyright © 2020 Dani. All rights reserved.
+//
+
+import UIKit
+
+class GFAvatarImageView: UIImageView {
+    
+    let placeholderImage = UIImage(named: "avatar-placeholder")
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        image = placeholderImage
+    }
+}
