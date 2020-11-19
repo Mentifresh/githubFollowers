5305ab3527033bfe7731a08c85f454e800c1333c
Daniel Kilders <dankil.dev@gmail.com>
2020-11-19 21:32:26 +0100

    Cleanup refactor

---
+++ githubFollowers/Custom Views/TabBarControllers/GFTabBarController.swift
@@ -0,0 +1,35 @@
+//
+//  GFTabBarController.swift
+//  githubFollowers
+//
+//  Created by Dani Kilders on 19.11.20.
+//  Copyright © 2020 Dani. All rights reserved.
+//
+
+import UIKit
+
+class GFTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        UITabBar.appearance().tintColor = .systemGreen
+        viewControllers = [createSearchNC(), createFavoritesListNC()]
+    }
+    
+    func createSearchNC() -> UINavigationController {
+        let searchVC = SearchVC()
+        searchVC.title = "Search"
+        searchVC.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
+        
+        return UINavigationController(rootViewController: searchVC)
+    }
+    
+    func createFavoritesListNC() -> UINavigationController {
+        let favoritesListVC = FavoritesListVC()
+        favoritesListVC.title = "Favorites"
+        favoritesListVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
+        
+        return UINavigationController(rootViewController: favoritesListVC)
+    }
+
+}
