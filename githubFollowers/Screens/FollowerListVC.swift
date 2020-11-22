77afd9cb514ab2b0e19c9f981c3ac32c79bbb7d2
Daniel Kilders <dankil.dev@gmail.com>
2020-11-23 00:14:03 +0100

    Fixed bug that would prevent to restore unfiltered users when previously filtered by 1 character

--- githubFollowers/Screens/FollowerListVC.swift
+++ githubFollowers/Screens/FollowerListVC.swift
@@ -105,7 +105,6 @@ class FollowerListVC: UIViewController {
     private func configureSearchController() {
         let searchController = UISearchController()
         searchController.searchResultsUpdater = self
-        searchController.searchBar.delegate = self
         searchController.searchBar.placeholder = "Search for a username"
         searchController.obscuresBackgroundDuringPresentation = false
         navigationItem.searchController = searchController
@@ -202,19 +201,19 @@ extension FollowerListVC: UICollectionViewDelegate {
 }
 
 
-extension FollowerListVC: UISearchResultsUpdating, UISearchBarDelegate {
+extension FollowerListVC: UISearchResultsUpdating {
     
     func updateSearchResults(for searchController: UISearchController) {
-        guard let filter = searchController.searchBar.text, !filter.isEmpty else { return }
+        guard let filter = searchController.searchBar.text, !filter.isEmpty else {
+            filteredFollowers.removeAll()
+            self.updateData(on: self.followers)
+            isSearching = false
+            return
+        }
         isSearching = true
         
         filteredFollowers = followers.filter { $0.login.lowercased().contains(filter.lowercased()) }
         updateData(on: filteredFollowers)
-    }
-    
-    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
-        isSearching = false
-        self.updateData(on: self.followers)
     }
 }
 
